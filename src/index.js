3fa294c56a3a34a81c7c77e821648526489e7f7b
oscardanil <oscardanilo_montenegro@outlook.com>
2021-11-13 08:31:49 -0600

    second Commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { env } from './config';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App env={env} />
   </React.StrictMode>,
   document.getElementById('root')
 );
